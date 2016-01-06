e32c29cd02158b44ea5291cf429e1ad432e1505e
David <david@robotics-unlimited.com>
2016-01-06 16:00:44 -0500

    add /todos n /todos/:id

--- server.js
+++ server.js
@@ -9,10 +9,31 @@ var todos = [{
     id:2,
     description: 'go to market',
     completed: false
+},{
+    id:3,
+    description: 'feed',
+    completed: true
 }];
 
 app.get('/',function(req,res){
     res.send('todo api root');
+});
+
+// GET /todos
+app.get('/todos',function(req,res){
+   res.json(todos);
+
+});
+
+//GET /todos/2
+app.get('/todos/:id',function(req,res){
+    var todoId = parseInt(req.params.id);
+    todos.forEach(function(todo){
+        if(todo.id === todoId){
+            return res.json(todo);
+        }
+    })
+    return res.status(404).send();
 });
 
 app.listen(PORT,function(){
