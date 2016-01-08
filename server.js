2b6eba48ed69640324615159fda24b9de4693828
David <david@robotics-unlimited.com>
2016-01-08 11:31:19 -0600

    refactored with underscore

--- server.js
+++ server.js
@@ -1,19 +1,17 @@
 var express = require('express');
 var app = express();
-var PORT = process.env.PORT || 3000
-var todos = [{
-    id: 1,
-    description: 'meet X for brunch',
-    completed: false
-},{
-    id:2,
-    description: 'go to market',
-    completed: false
-},{
-    id:3,
-    description: 'feed',
-    completed: true
-}];
+var PORT = process.env.PORT || 3000;
+var bodyParser = require('body-parser');
+var _ = require("underscore");
+
+var todos = [];
+var todoNextId = 1;
+
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
+    extended: true
+}));
 
 app.get('/',function(req,res){
     res.send('todo api root');
@@ -27,15 +25,31 @@ app.get('/todos',function(req,res){
 
 //GET /todos/2
 app.get('/todos/:id',function(req,res){
-    var todoId = parseInt(req.params.id);
-    todos.forEach(function(todo){
-        if(todo.id === todoId){
-            return res.json(todo);
-        }
-    })
-    return res.status(404).send();
+    var todoId = parseInt(req.params.id,10);
+    // find the todo in the todos array
+    var matchedToDo = _.findWhere(todos,{id:todoId});
+
+    if(matchedToDo){
+        return res.json(matchedToDo);
+    }else
+        return res.status(404).send();
+});
+
+//POST  /todos/
+
+app.post('/todos', function(req,res){
+    var body = req.body;
+
+    if(_.isBoolean(body.completed) || !_.isString(body.description) || body.description.trim().length === 0 ){
+        return res.status(400).send();
+    }
+    body.id = todoNextId++;
+    todos.push(body);
+
+    console.log('description ' + body.description);
+    res.json(body);
 });
 
 app.listen(PORT,function(){
-    console.log('express listenning on port ' +  PORT + '!');
+    console.log('express listening on port ' +  PORT + '!');
 });
