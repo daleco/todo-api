13d970ac4a4e6ebcb28452d6f7983cc099516952
David <david@robotics-unlimited.com>
2016-01-08 14:26:51 -0600

    add PUT/todos/:id

--- server.js
+++ server.js
@@ -39,7 +39,7 @@ app.post('/todos', function(req,res){
     var body = _.pick(req.body,'description','completed');
     // use .pick to get only description and completed
 
-    if(_.isBoolean(body.completed) || !_.isString(body.description) || body.description.trim().length === 0 ){
+    if(!_.isBoolean(body.completed) || !_.isString(body.description) || body.description.trim().length === 0 ){
         return res.status(400).send();
     }
 
@@ -61,8 +61,35 @@ app.delete('/todos/:id',function(req,res){
         todos = _.without(todos, matchToDo);
         res.json(matchToDo);
     }
-
 });
+
+// /PUT/todos/:id
+
+app.put('/todos/:id', function(req,res){
+    var idToDo = parseInt(req.params.id,10);
+    var matchToDo = _.findWhere(todos,{id:idToDo});
+    var body = _.pick(req.body,'description','completed');
+    var validAttributes = {};
+
+    if(!matchToDo) return res.status(404).send();
+
+    if(body.hasOwnProperty('completed') && _.isBoolean(body.completed)){
+        validAttributes.completed = body.completed;
+    }else if(body.hasOwnProperty('completed')){
+            return res.status(400).send();
+        }else{
+
+        }
+
+    if(body.hasOwnProperty('description') && _.isString(body.description) && body.description.trim().length >0){
+        validAttributes.description = body.description;
+    }else if(body.hasOwnProperty('description')){
+        return res.status(400).send();
+    }
+
+    _.extend(matchToDo,validAttributes);
+    res.json(matchToDo);
+})
 
 
 app.listen(PORT,function(){
