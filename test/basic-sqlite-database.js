a084d674cfb96fe874ac5a6ddaf636b3208115d2
David <david@robotics-unlimited.com>
2016-01-12 13:53:41 -0600

    add sequel

--- test/basic-sqlite-database.js
+++ test/basic-sqlite-database.js
@@ -6,19 +6,57 @@ var sequelize = new Sequelize(undefined, undefined, undefined,{
 
 var Todo = sequelize.define('todo',{
     description:{
-        type:Sequelize.STRING
+        type:Sequelize.STRING,
+        allowNull: false,
+        validate:{
+            len:[1,250]
+        }
     },
     completed:{
-        type: Sequelize.BOOLEAN
+        type: Sequelize.BOOLEAN,
+        allowNull: false,
+        defaultValue: false
     }
 })
-sequelize.sync({force: true}).then(function () {
+sequelize.sync({
+   // force: true
+}).then(function () {
     console.log('Everything is synced');
-    Todo.create({
-        description: 'Walking my dog',
-        completed: false
-    }).then(function(todo){
-        console.log("Finished!");
-        console.log('todo');
+
+    Todo.findById(2).then(function(todo){
+        if(todo)
+            console.log(todo.toJSON());
+        else
+            console.log("todo not found");
+    }).catch(function(e){
+        console.log(e);
     })
+
+    /*
+    Todo.create({
+        description : 'takeout pizza'
+    }).then(function(todo){
+        return Todo.create({
+            description: 'pick up trash'
+        });
+        console.log("Finished!");
+    }).then(function(){
+        return Todo.findAll({
+        where:{
+            description:{
+                $like: '%pizza%'
+            }
+        }
+        });
+    }).then(function(todos){
+        if(todos){
+            todos.forEach(function(todo){
+                console.log(todo.toJSON());
+            });
+        }else{
+            console.log('no todo found');
+        }
+    }).catch(function(e){
+        console.log(e);
+    })*/
 });
